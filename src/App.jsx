f7fc6c10b3025e3e491120fd1a20d2004e2a631b
Chandan Raj <chandanraaz91@gmail.com>
2024-09-04 13:15:56 +0530

    user registration completed

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/loginPgae";
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFound from "./pages/NotFound";
+import RootLayout from "./shared/Layout/RootLayout";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<RootLayout><HomePage /></RootLayout>} />
+          <Route path='/blog' element={<RootLayout><HomePage /></RootLayout>} />
+          <Route path='/login' element={<RootLayout><LoginPage /></RootLayout>} />
+          <Route path='/signup' element={<RootLayout><SignupPage /></RootLayout>} />
+          <Route path='/dashboard' element={<RootLayout><DashboardPage /></RootLayout>} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,16 @@
+import { Watermark } from "antd"
+
+const DashboardPage = () => {
+  return (
+    <>
+    <Watermark content="Dashboard">
+        <div
+          style={{
+            height: '100vh',
+          }}
+        />
+      </Watermark></>
+  )
+}
+
+export default DashboardPage
